641f72864a2803f6054544f7959a6d4f077d0bb7
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-10-18 11:37:48 -0400

    init

---
+++ menu.rb
@@ -0,0 +1,8 @@
+class Menu
+  def initialize(menu_prices, target_price)
+    @menu_prices = menu_prices
+    @target_price = target_price
+  end
+
+  
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Menu do
+  describe '#call' do
+    it 'test' do
+      expect(1).to eq(2)
+    end
+  end
+end
