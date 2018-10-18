8cce4634590ae1a0ec57defe5f9f665ddd1712e8
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-10-18 14:07:20 -0400

    passing specs

--- menu_combinations.rb
+++ menu_combinations.rb
@@ -1,32 +1,33 @@
 class MenuCombinations
-  def self.call(file)
-    new(file).call
+  attr_reader :target_price, :menu_items
+
+  def self.call(file_path)
+    new(file_path).call
   end
 
-  def initialize(file)
-    parse_file(file)
+  def initialize(file_path)
+    @file_path = file_path
+    parse_file
   end
 
-  def parse_file(file)
+  def parse_file
+    file = File.open(@file_path)
+    @menu_items = Hash.new
 
+    file.each_line.with_index do |line, i|
+      line = line.sub("$", "")
+
+      if i == 0
+        @target_price = line.to_f
+      else
+        menu_item = line.split(",")
+        item = menu_item.first.gsub(" ", "_")
+        price = menu_item.last.to_f
+
+        @menu_items[item] = price
+      end
+    end
   end
   private :parse_file
 end
-
-# module MenuCombinations
-#   def call
-# end
-# class FileReader
-#
-# class Menu
-#   def initialize(file)
-#
-#   end
-#
-#   def initialize(menu_prices, target_price)
-#     @menu_prices = menu_prices
-#     @target_price = target_price
-#   end
-#
-#
-# end
+menu = MenuCombinations.new("test_menu.txt")
--- spec/menu_combinations_spec.rb
+++ spec/menu_combinations_spec.rb
@@ -4,19 +4,25 @@ require 'tempfile'
 describe MenuCombinations do
   describe "#parse_file" do
     describe 'when the file is formatted correctly' do
-      let(:file) do
-        Tempfile.new(
-          "$15.05
-          mixed fruit,$2.15
-          french fries,$2.75
-          side salad,$3.35
-          hot wings,$3.55
-          mozzarella sticks,$4.20
-          sampler plate,$5.80"
-        )
+      let(:file_name) { "menu" }
+
+      let(:file_data) do
+         <<-FILE_DATA
+$15.05
+mixed fruit,$2.15
+french fries,$2.75
+side salad,$3.35
+hot wings,$3.55
+mozzarella sticks,$4.20
+sampler plate,$5.80"
+FILE_DATA
       end
 
-      let(:menu_combinations) { MenuCombinations.new(file) }
+      before(:each) do
+        allow(File).to receive(:open).with(file_name).and_return(StringIO.new(file_data))
+      end
+
+      let(:menu_combinations) { MenuCombinations.new(file_name) }
 
       it 'parses the total price' do
         expect(menu_combinations.target_price).to eq(15.05)
@@ -25,12 +31,12 @@ describe MenuCombinations do
       it 'parses the menu items' do
         expect(menu_combinations.menu_items).to eq(
           {
-            mixed_fruit: 2.15,
-            french_fries: 2.75,
-            side_salad: 3.35,
-            hot_wings: 3.55,
-            mozzarella_sticks: 4.2,
-            sampler_plate: 5.8
+            "mixed_fruit" => 2.15,
+            "french_fries" => 2.75,
+            "side_salad" => 3.35,
+            "hot_wings" => 3.55,
+            "mozzarella_sticks" => 4.2,
+            "sampler_plate" => 5.8
           }
         )
       end
